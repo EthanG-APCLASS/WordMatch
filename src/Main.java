8b972d9510dd4e27322ba91fd47cafcf5bdf9430
Ethan Gao <Ethang152@nycstudents.net>
2025-10-14 15:33:56 -0400

    WIP CODE

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,5 @@ public class Main
         System.out.println(p.scoreGuess("aabb"));
         System.out.println(p.scoreGuess("c"));
         WordMatch game = new WordMatch("concatenation");
-
     }
 }
