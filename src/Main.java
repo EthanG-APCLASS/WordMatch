d4ed95bc30838b56667b80f87684691bee392aea
Ethan Gao <Ethang152@nycstudents.net>
2025-10-14 15:33:33 -0400

    WIP CODE

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main
+{
+    public static void main (String[] args)
+    {
+        WordMatch w = new WordMatch("mississippi");
+        System.out.println(w.scoreGuess("i"));
+        System.out.println(w.scoreGuess("iss"));
+        System.out.println(w.scoreGuess("issipp"));
+        System.out.println(w.scoreGuess("mississippi"));
+        WordMatch p = new WordMatch("aaaabb");
+        System.out.println(p.scoreGuess("a"));
+        System.out.println(p.scoreGuess("aa"));
+        System.out.println(p.scoreGuess("aaa"));
+        System.out.println(p.scoreGuess("aabb"));
+        System.out.println(p.scoreGuess("c"));
+        WordMatch game = new WordMatch("concatenation");
+
+    }
+}
