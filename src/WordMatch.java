6e636982eb162bbb64b85a8cf021bf2f2441d30b
Ethan Gao <Ethang152@nycstudents.net>
2025-10-16 15:21:25 -0400

    FINISHED AHHH

--- src/WordMatch.java
+++ src/WordMatch.java
@@ -17,7 +17,13 @@
         public String findBetterGuess(String guess1, String guess2)
         {
             if (scoreGuess(guess1) > scoreGuess(guess2))
+            {
                 return guess1;
+            }
+            if (scoreGuess(guess1) < scoreGuess(guess2))
+            {
+                return guess2;
+            }
             if (guess1.compareTo(guess2) > 0) return guess1;
             return guess2;
         }
